c0518ad4807e5367e2e6547fec8fb0498897e74a
TPhuongBui <thanhphuong.bui2018@gmail.com>
2022-10-19 17:12:27 +0700

    Tạo mới phần bài tập js buổi 4

---
+++ js/bai1.js
@@ -0,0 +1,11 @@
+function sapXepTangDan() {
+    var num1 = document.getElementById("inputSoThuNhat").value;
+    var num2 = document.getElementById("inputSoThuHai").value;
+    var num3 = document.getElementById("inputSoThuBa").value;
+
+    var thuTu = "";
+
+    if (num1 > num2 && num1 > num3) {
+    }
+
+}
